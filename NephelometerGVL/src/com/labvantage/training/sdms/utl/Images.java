8a34e4cb57ce5e8ffafdac13ca03c5f01d0ee01f
glims-bausch <prisca.cao@bausch.com>
2026-01-07 15:15:11 -0500

    Add files via upload

---
+++ NephelometerGVL/src/com/labvantage/training/sdms/utl/Images.java
@@ -0,0 +1,19 @@
+package com.labvantage.training.sdms.utl;
+
+public class Images {
+	byte[] primaryImage;
+	byte[] secondaryImage;
+	public byte[] getPrimaryImage() {
+		return primaryImage;
+	}
+	public void setPrimaryImage(byte[] primaryImage) {
+		this.primaryImage = primaryImage;
+	}
+	public byte[] getSecondaryImage() {
+		return secondaryImage;
+	}
+	public void setSecondaryImage(byte[] secondaryImage) {
+		this.secondaryImage = secondaryImage;
+	}
+	
+}
